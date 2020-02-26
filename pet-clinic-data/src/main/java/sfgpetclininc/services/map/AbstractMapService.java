94650685be6d35977622736f9696efef7a2b1456
esha <eshu90123@gmail.com>
2020-02-26 17:00:52 +0530

    Impl Map based Service , Closes issue#6.

---
+++ pet-clinic-data/src/main/java/sfgpetclininc/services/map/AbstractMapService.java
@@ -0,0 +1,33 @@
+package sfgpetclininc.services.map;
+
+import com.sun.xml.bind.v2.model.core.ID;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T, ID> {
+
+    protected Map<ID, T> map = new HashMap<>();
+
+   public Set<T> findAll(){
+        return new HashSet<>(map.values());
+    }
+
+   public T findById(ID id){
+        return map.get(id);
+    }
+
+    public T save(ID id, T object){
+        return map.put(id, object);
+    }
+
+    public void deleteById(ID id){
+        map.remove(id);
+    }
+
+    public void delete(T object){
+        map.entrySet().removeIf(entry ->entry.getValue().equals(object));
+    }
+}
