4b96ce2c986c53d9e408b3e55c0d7d715342566f
esha <eshu90123@gmail.com>
2020-02-28 10:29:55 +0530

    Added visits and updated owner with pet. Closes #26

--- pet-clinic-data/src/main/java/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/model/PetType.java
@@ -1,4 +1,14 @@
 package sfgpetclinic.model;
 
 public class PetType extends BaseEntity{
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
 }
