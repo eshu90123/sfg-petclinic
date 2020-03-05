e1b95b733726433774d61cf03e30e9e418839ba3
esha <eshu90123@gmail.com>
2020-03-05 14:49:22 +0530

    implemented create visit test class

--- pet-clinic-web/src/main/java/sfgpetclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/controllers/VisitController.java
@@ -11,6 +11,8 @@ import sfgpetclinic.services.PetService;
 import sfgpetclinic.services.VisitService;
 
 import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 import java.util.Map;
 
 @Controller
@@ -28,6 +30,12 @@ public class VisitController {
     @InitBinder
     public void setAllowedFields(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
+       /* dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });*/
     }
 
     /**
